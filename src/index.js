70fa60378bc9272817f3b2156d8bb4d85543d471
Bharat Singh Rathore <mrbharatsingh4@gmail.com>
2022-07-26 17:41:30 +0530

    cours

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const express=require("express")
+const cors=require("cors")
 const app=express()
 
-// const cors=require("cors")
-app.use(express.json());
 app.use(cors())
+app.use(express.json());
+
 console.log("index.js")
 
 const locationController=require("./controller/location.controller")
